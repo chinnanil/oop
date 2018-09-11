81d8fcbcc55c433163633f8704c4ad4a72cebfb2
anil <meghanilva@live.in>
2018-09-11 21:20:47 +0530

    class Account created

---
+++ oop.py
@@ -0,0 +1,15 @@
+class Account:
+    interest = 0.01
+    def __init__(self,holder):
+        self.holder=holder
+        self.balance=0
+
+    def deposit(self,amount):
+        self.balance+=amount
+        return self.balance
+    def withdraw(self,amount):
+        if amount <= self.balance:
+            self.balance-=amount
+            return self.balance
+        else :
+            return "insufficient balance"
