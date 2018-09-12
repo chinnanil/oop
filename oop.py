415d401e852ba33412e1efef73c1f758082fa7ef
anil <meghanilva@live.in>
2018-09-12 21:19:11 +0530

    balance to blance'

--- oop.py
+++ oop.py
@@ -12,4 +12,4 @@ class Account:
             self.balance-=amount
             return self.balance
         else :
-            return "insufficient balance"
+            return "insufficient blance"
